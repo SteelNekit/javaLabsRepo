2825b7a77e1de2c4898a2a2df060f07f9d43bde0
SteelNekit <62691258+SteelNekit@users.noreply.github.com>
2020-03-26 12:02:00 +0400

    Update Utils.java

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Utils {
     //Ебать, спросить почему эта хуета не фурычит
-    //[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{2,3}
+    //[ABEKMHOPCTYX]d{3}[ABEKMHOPCTYX]{2}d{2,3}
     public static String checkRegNumberFormat(String regNumber){
         if(!Pattern.matches("[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}[0-9]{2,3}", regNumber)) throw new RegistrationNumberFormatException();
         return regNumber;
